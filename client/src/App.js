9aa364ac76ac236209e13b08bbd889391792e04a
Ningjia Huang <nxh239@case.edu>
2021-05-08 04:57:02 -0400

    Navbar added.

--- client/src/App.js
+++ client/src/App.js
@@ -67,7 +67,15 @@ function App() {
                   )
                 }
               />
-              <Route
+
+              <Route exact path="/login/parent" render={props => !isAuthenticated ? <ParentLogin {...props} setAuth={setAuth}/> : <Redirect to="/parent_dashboard"/>}/>
+              <Route exact path="/register/parent" render={props => !isAuthenticated ? <ParentRegister {...props} setAuth={setAuth}/> : <Redirect to="/login/parent"/>}/>
+              <Route exact path="/parent_dashboard" render={props => isAuthenticated ? <ParentDashboard {...props} setAuth={setAuth}/>: <Redirect to="/login/parent"/>}/>
+              <Route exact path="/login/admin" render={props => !isAuthenticated ? <AdminLogin {...props} setAuth={setAuth}/> : <Redirect to="/admin_dashboard"/>}/>
+              <Route exact path="/admin_dashboard" render={props => isAuthenticated ? <AdminDashboard {...props} setAuth={setAuth}/>: <Redirect to="/login/admin"/>}/>
+            </Switch>
+          </div>
+          <Route
                exact 
                path="/breeder_dashboard" 
                render={props => 
@@ -78,13 +86,6 @@ function App() {
                 )
               }
               />
-              <Route exact path="/login/parent" render={props => !isAuthenticated ? <ParentLogin {...props} setAuth={setAuth}/> : <Redirect to="/parent_dashboard"/>}/>
-              <Route exact path="/register/parent" render={props => !isAuthenticated ? <ParentRegister {...props} setAuth={setAuth}/> : <Redirect to="/login/parent"/>}/>
-              <Route exact path="/parent_dashboard" render={props => isAuthenticated ? <ParentDashboard {...props} setAuth={setAuth}/>: <Redirect to="/login/parent"/>}/>
-              <Route exact path="/login/admin" render={props => !isAuthenticated ? <AdminLogin {...props} setAuth={setAuth}/> : <Redirect to="/admin_dashboard"/>}/>
-              <Route exact path="/admin_dashboard" render={props => isAuthenticated ? <AdminDashboard {...props} setAuth={setAuth}/>: <Redirect to="/login/admin"/>}/>
-            </Switch>
-          </div>
           <div>
 
           </div>
