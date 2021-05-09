4e46e2f27fb61e3487e536ce8a299f67fe242323
jyyy03 <57975957+jyyy03@users.noreply.github.com>
2021-05-09 10:18:13 -0400

    Add files from original repo

---
+++ client/src/components/SearchCattery.js
@@ -0,0 +1,61 @@
+import React, {Fragment, useState, useEffect} from 'react'
+import mock_data from "./MOCK_DATA.json"
+import { Button } from 'react-bootstrap';
+
+const SearchCattery = () =>{
+  const[searchTerm, setSearchTerm] = useState("")
+  const[catteries, setCatteries] = useState([]);
+  const getCatteries = async() => {
+      try{
+          
+          const response = await fetch("http://localhost:4020/api/v1/catteries")
+          const jsonData = await response.json()
+          setCatteries(jsonData);
+
+      } catch (err){
+          console.error(err.message);
+      }
+  }
+
+  useEffect(() =>{
+      getCatteries();
+  }, [])
+
+    return(
+        <Fragment>
+             {" "}
+          <input type="text" className="form-control" 
+          placeholder="search cattery here" 
+          onChange={event => {setSearchTerm(event.target.value)}}
+          />
+          <table class="table mt-5 text-center">
+              <thead>
+                  <tr>
+                      <th>Cattery Name</th>
+                      <th>Owner Name</th>
+                  </tr>
+              </thead>
+              <tbody>
+              {mock_data.filter((val) =>{  //if get all catteries completed, change "mock_data" to catteries
+                  if (searchTerm == ""){
+                    return val
+                  } else if (val.cattery_name.toLowerCase().includes(searchTerm.toLowerCase()) ){
+                    return val
+                  }
+              }
+              ).map((val, key) => (
+                  <tr key={key}>
+                      <td>{val.cattery_name}</td>
+                      <td>{val.owner_name}</td>
+                      <td>
+                      </td>
+                  </tr>
+              ))
+              }
+            </tbody>
+        </table>
+        </Fragment>
+    )
+}
+
+export default SearchCattery;
