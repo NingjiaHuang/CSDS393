f14461de83833f0a2cf73c703b424c0512a3ea2e
jyyy03 <57975957+jyyy03@users.noreply.github.com>
2021-05-14 15:15:49 -0400

    login test completed w/o fetch method.

--- client/src/components/__tests__/cattery.test.js
+++ client/src/components/__tests__/cattery.test.js
@@ -3,20 +3,6 @@ import SearchCattery from '../SearchCattery';
 import renderer from 'react-test-renderer'
 import '@testing-library/jest-dom/extend-expect';
 
-
-const unmockedFetch = global.fetch
-
-beforeAll(() => {
-  global.fetch = () =>
-    Promise.resolve({
-      json: () => Promise.resolve([]),
-    })
-})
-
-afterAll(() => {
-  global.fetch = unmockedFetch
-})
-
 test('matches snapshot of search cattery', () =>{
     const tree = renderer.create(<SearchCattery/>).toJSON();
     expect(tree).toMatchSnapshot();
--- client/src/components/__tests__/login.test.js
+++ client/src/components/__tests__/login.test.js
@@ -24,3 +24,51 @@ test('matches snapshot of breeder register', () =>{
     const tree = renderer.create(<AdminLogin/>).toJSON();
     expect(tree).toMatchSnapshot();
 })
+
+describe("login admin", () =>{
+    describe("with valid inputs", () =>{
+    it('changes the input', async() =>{
+        const mockonsubmit = jest.fn()
+        const {getByLabelText} = render(<AdminLogin onsubmit={mockonsubmit}/>)
+        const password = getByLabelText("Password:")
+        const email = getByLabelText("Email:")
+        fireEvent.change(email, {target:{value:"test@test.com"}})
+        fireEvent.change(password, {target:{value:"test"}})
+
+        expect(password.value).toMatch("test")
+        expect(email.value).toMatch("test@test.com")
+    })
+    })
+})
+
+describe("login breeder", () =>{
+    describe("with valid inputs", () =>{
+    it('changes the input', async() =>{
+        const mockonsubmit = jest.fn()
+        const {getByLabelText} = render(<BreederLogin onsubmit={mockonsubmit}/>)
+        const password = getByLabelText("Password:")
+        const email = getByLabelText("Email:")
+        fireEvent.change(email, {target:{value:"test@test.com"}})
+        fireEvent.change(password, {target:{value:"test"}})
+
+        expect(password.value).toMatch("test")
+        expect(email.value).toMatch("test@test.com")
+    })
+    })
+})
+
+describe("login admin", () =>{
+    describe("with valid inputs", () =>{
+    it('changes the input', async() =>{
+        const mockonsubmit = jest.fn()
+        const {getByLabelText} = render(<ParentLogin onsubmit={mockonsubmit}/>)
+        const password = getByLabelText("Password:")
+        const email = getByLabelText("Email:")
+        fireEvent.change(email, {target:{value:"test@test.com"}})
+        fireEvent.change(password, {target:{value:"test"}})
+
+        expect(password.value).toMatch("test")
+        expect(email.value).toMatch("test@test.com")
+    })
+    })
+})
